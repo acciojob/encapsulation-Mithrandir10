2db6026abbba1b492d4a0a66ba02aa5020282277
parteek singh <sparteek9@gmail.com>
2023-04-13 22:20:16 +0530

    added func

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String input=sc.next();
+        RWOnly obj1=new RWOnly();
+        obj1.setName(input);
+        System.out.println(obj1.getName());
+    }
   
 }
